50c2fcdc750205ac16c62f4f7ae88421480be58e
exsaber <1960028060@qq.com>
2014-01-20 15:44:23 +0800

    rename my-lisps ==> settings

--- init/init-path.el
+++ init/init-path.el
@@ -32,11 +32,11 @@
 ;;; Code:
 
 (defconst my-emacs-path "~/.emacs.d/" "我的emacs相关配置文件的路径")
-(defconst my-emacs-my-lisps-path  (concat my-emacs-path "my-lisps/") "我自己写的emacs lisp包的路径")
+(defconst my-emacs-my-lisps-path  (concat my-emacs-path "settings/") "我自己的配置的路径")
 (defconst my-emacs-lisps-path     (concat my-emacs-path "lisps/") "我下载的emacs lisp包的路径")
 (defconst my-emacs-templates-path (concat my-emacs-path "templates/") "Path for templates")
 (defconst my-emacs-libs (concat my-emacs-path "libs/") "我的emacs库")
-(defconst my-emacs-faces (concat my-emacs-path "faces/") "我的emacs库")
+(defconst my-emacs-faces (concat my-emacs-path "faces/") "我的faces配置")
 
 ;; 把`my-emacs-lisps-path'的所有子目录都加到`load-path'里面
 (load (concat my-emacs-libs "my-subdirs"))
