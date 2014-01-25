1fca83c9a3854f45de39241e90efd8b7f6208e6f
yubin <1960028060@qq.com>
2014-01-25 15:56:36 +0800

    fix fonts on linux

--- settings/font-settings.el
+++ settings/font-settings.el
@@ -1,15 +1,15 @@
 ;; -*- Emacs-Lisp -*-
 
-;; Time-stamp: <2013-12-21 17:25:22 Saturday by yubin>
-;(when (and window-system is-after-emacs-23)
-;  (require 'my-fontset-win)
-;  (if mswin
-;      ;; (huangq-fontset-courier 17)
-;      (huangq-fontset-consolas 17)
-;    ;; (huangq-fontset-dejavu 17)))
-;    ;; (huangq-fontset-fixedsys 17)
-;    (set-default-font "Monospace 11")
-; ))
+;; Time-stamp: <2014-01-25 15:54:20 Saturday by yubin>
+(unless (eq window-system 'w32)
+ (when (and window-system is-after-emacs-23)
+  (require 'my-fontset-win)
+  (if mswin
+      ;; (huangq-fontset-courier 17)
+      (huangq-fontset-consolas 17)
+    ;; (huangq-fontset-dejavu 17)))
+    ;; (huangq-fontset-fixedsys 17)
+    (set-default-font "Monospace 11"))))
 
 (when (eq window-system 'w32)
    (set-face-attribute
