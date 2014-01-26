60eb159bfad2a1fe7de58fceaaf5aea49607a29a
exsaber <1960028060@qq.com>
2014-01-26 13:45:11 +0800

    add desktop

--- init.el
+++ init.el
@@ -1,12 +1,15 @@
 ;; -*- Emacs-Lisp -*-
 
-;; Time-stamp: <2014-01-23 17:53:13 Thursday by yubin>
+;; Time-stamp: <2014-01-26 13:39:25 Sunday by yubin>
 
 (add-to-list 'load-path (expand-file-name "~/.emacs.d"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/init"))
 
+
 (require 'init-path)
 
+;; Measure startup time
+(require 'init-time)
 ;;my
 (require 'init-my)
 ;;buildin-package
@@ -173,12 +176,15 @@
 (require 'init-keybind)
 
 ;; 王纯业的desktop, 比desktop快多了
-(require 'wcy-desktop-settings)
+;(require 'wcy-desktop-settings)
 
 ;; HACK: 要放在最后,免得会出现比较奇怪的现象
 ;; 保存和恢复工作环境
 ;; desktop,用来保存Emacs的桌面环境 — buffers、以及buffer的文件名、major modes和位置等等
-;; (require 'desktop-settings)
+(require 'desktop-settings)
 
 ;; 加载用户自己的配置
 (require 'init-custom)
+
+(message "init completed in %.2fms"
+         (exsaber/time-subtract-millis (current-time) before-init-time))
--- settings/desktop-settings.el
+++ settings/desktop-settings.el
@@ -1,12 +1,34 @@
 ;; -*- Emacs-Lisp -*-
 
-;; Time-stamp: <2014-01-23 17:39:28 Thursday by yubin>
+;; Time-stamp: <2014-01-26 13:39:40 Sunday by yubin>
 
 (eval-after-load "desktop"
   '(progn
 
+     (defadvice desktop-read (around trace-desktop-errors activate)
+       (let ((debug-on-error t))
+         ad-do-it))
+
+     (defadvice desktop-read (around time-restore activate)
+       (let ((start-time (current-time)))
+         (prog1
+             ad-do-it
+           (message "Desktop restored in %.2fms"
+                    (exsaber/time-subtract-millis (current-time)
+                                                  start-time)))))
+
+     (defadvice desktop-create-buffer (around time-create activate)
+       (let ((start-time (current-time))
+             (filename (ad-get-arg 1)))
+         (prog1
+             ad-do-it
+           (message "Desktop: %.2fms to restore %s"
+                    (exsaber/time-subtract-millis (current-time)
+                                                  start-time)
+                    (when filename
+                      (abbreviate-file-name filename))))))
+
      (setq desktop-load-locked-desktop t)
-     (if is-before-emacs-21 (desktop-load-default) (desktop-save-mode 1))
 
      (dolist (var (list 'command-history 'kill-ring 'file-name-history 'find-symbol-last-symbol
                         'extended-command-history 'grep-history 'compile-history 'last-template
@@ -18,6 +40,8 @@
      (apply-define-key
       global-map
       `(("C-x M-C" desktop-clear)))))
+
+(setq desktop-path (list user-emacs-directory))
 
 (desktop-save-mode 1)
 
